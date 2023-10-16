b931e38ce8c56b33bcd58f59cdd9c3b1a3e74542
ameliesiquet <amelie.siquet@student.hepl.be>
2023-10-16 19:51:28 +0200

    fonctionne

--- OCR-starter/chapitre3_exercice3.js
+++ OCR-starter/chapitre3_exercice3.js
@@ -7,3 +7,10 @@
     * les secondes actuelles
 - … (TODO)
 */
+const heureactuelle = new Date;
+
+const heures = heureactuelle.getHours();
+const minutes =heureactuelle.getMinutes();
+const secondes = heureactuelle.getSeconds();
+
+console.log(`Temps actuel:${heures},${minutes},${secondes}`)
