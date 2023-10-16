01cf93948c31c0f764afee22d9f07095b99c2492
ameliesiquet <amelie.siquet@student.hepl.be>
2023-10-16 19:23:50 +0200

    fonctionne

--- OCR-starter/chapitre3_exercice2.js
+++ OCR-starter/chapitre3_exercice2.js
@@ -7,3 +7,14 @@
 	* "Le candidat est reçu" si la cote est entre 10 et 12
 	* "Le candidat est reçu avec mention" si la cote est supérieur ou égale à 12
 */
+const moyenne = 9;
+
+if (moyenne<10){
+    console.log("Le candidat est recalé");
+}
+else if(moyenne>=10 && moyenne <12){
+    console.log("Le candidat est reçu");
+}
+else if (moyenne >= 12 ) {
+    console.log("Le candidat est reçu avec mention");
+}
