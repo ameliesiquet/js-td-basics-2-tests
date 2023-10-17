0902c41271183b3a64fc8a0fa339ab9dfb334494
ameliesiquet <amelie.siquet@student.hepl.be>
2023-10-17 20:34:21 +0200

    fonctionne

--- starter/tests-p1.js
+++ starter/tests-p1.js
@@ -3,3 +3,16 @@ TESTS - PRÉPA 1
 Ecrire un programme JS qui calcule la facture d’un client qui achète N articles vendus au prix x
 sachant qu’on accorde 10% de ristourne si le client achète au moins 20 articles.
 */
+const articles ="56";
+const prix= "50";
+const prixtotal= articles*prix;
+const ristourne = prixtotal*0.1;
+
+if (prixtotal>=20){
+    prixfinal=ristourne
+}
+console.log(`Le montant total de la facture est ${prixfinal}`);
+
+
+
+
