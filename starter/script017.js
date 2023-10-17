243c31394d1de4ab8d2dab79b2ca3cb05b92b093
ameliesiquet <amelie.siquet@student.hepl.be>
2023-10-17 18:05:27 +0200

    fonctionne

--- starter/script017.js
+++ starter/script017.js
@@ -13,6 +13,14 @@
 3. Testez si la variable height est définie et affichez "La variable height est définie" ou "La variable height n'est PAS définie" selon le cas
 4. Testez différents cas de figure en changeant la valeur de height (par exemple 0, ou NaN) ou en ne lui affectant pas de valeur… ATTENTION, votre script doit bien dire que "La variable height est définie" dans le cas où on a donné à height une valeur de 0…
 */
+const height= NaN;
+if (height || height ===0){
+    console.log("La variable height es définie");
+}else {
+    console.log("La variable height n'est PAS définie");
+}
+
+
 
 
 
@@ -22,6 +30,13 @@
 3. Écrivez le test qui permet d'afficher, selon le cas, "Cette variable contient bien une valeur non nulle" ou "Cette variable est nulle ou non définie"
 4. Testez différents cas de figure en changeant la valeur de height ou en ne lui affectant pas de valeur…
 */
+const height2 = 20;
+if (height2 !== undefined && height2 !== 0) {
+    console.log("Cette variable contient bien une valeur non nulle");
+} else {
+    console.log("Cette variable est nulle ou non définie");
+}
+
 
 
 
@@ -32,4 +47,8 @@
 1. Écrivrez une instruction conditionnelle qui affiche bien a propos "J'ai testé si 23 était égal à '23' sans vérifier le type. L'opérateur que 'ai utilisé a opéré une conversion de type"
 2. Changez ensuite en utilisant l'opérateur dégalité stricte pour vérifier que cette phrase ne s'affiche plus
 */
+if (23 === `23`) {
+    console.log("J'ai testé si 23 était égal à '23' sans vérifier le type. L'opérateur que 'ai utilisé a opéré une conversion de type")
+}else{
 
+}
